fac60ed4699186da08f56ffe634ab00a6d6c89bb
hoang <nh231296@gmail.com>
2023-12-12 13:03:13 +0700

    LOG

--- setup.sh
+++ setup.sh
@@ -85,7 +85,7 @@ setup_compose() {
   # sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
   curl -OL https://raw.githubusercontent.com/M3m3M4n/RedashSetup/master/data/docker-compose.yml
   curl -OL https://raw.githubusercontent.com/M3m3M4n/RedashSetup/master/data/Dockerfile
-  if [ -z "$1" ]; then
+  if [ $# -lt 1 ]; then
     IP=$(hostname -I | awk '{print $1}')
   else
     IP=$1
